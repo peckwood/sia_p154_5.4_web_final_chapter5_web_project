f30f7a34ddbcbdfe58215692be2ea00d761c0852
raiden <peckwood@users.noreply.github.com>
2017-07-08 17:45:54 +0800

    initial commit, the validation in test is still not working

---
+++ src/test/java/spittr/web/SpitterControllerTest.java
@@ -0,0 +1,71 @@
+package spittr.web;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import spittr.Spitter;
+import spittr.config.RootConfig;
+import spittr.config.WebConfig;
+import spittr.data.SpitterRepository;
+import spittr.web.SpitterController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes={RootConfig.class, WebConfig.class})
+public class SpitterControllerTest {
+
+  @Test
+  public void shouldShowRegistration() throws Exception {
+    SpitterRepository mockRepository = mock(SpitterRepository.class);
+    SpitterController controller = new SpitterController(mockRepository);
+    MockMvc mockMvc = standaloneSetup(controller).build();
+    mockMvc.perform(get("/spitter/register"))
+           .andExpect(view().name("registerForm"));
+  }
+  
+  @Test
+  public void shouldProcessRegistration() throws Exception {
+    SpitterRepository mockRepository = mock(SpitterRepository.class);
+    Spitter unsaved = new Spitter("jbauer", "24hours", "Jack", "Bauer", "jbauer@ctu.gov");
+    Spitter saved = new Spitter(24L, "jbauer", "24hours", "Jack", "Bauer", "jbauer@ctu.gov");
+    when(mockRepository.save(unsaved)).thenReturn(saved);
+    
+    SpitterController controller = new SpitterController(mockRepository);
+    MockMvc mockMvc = standaloneSetup(controller).build();
+
+    mockMvc.perform(post("/spitter/register")
+           .param("firstName", "Jack")
+           .param("lastName", "Bauer")
+           .param("username", "jbauer")
+           .param("password", "24hours")
+           .param("email", "jbauer@ctu.gov"))
+           .andExpect(redirectedUrl("/spitter/jbauer"));
+    
+    verify(mockRepository, atLeastOnce()).save(unsaved);
+  }
+
+  //validation is not working, but only in @Test
+  @Test
+  public void shouldFailValidationWithNoData() throws Exception {
+    SpitterRepository mockRepository = mock(SpitterRepository.class);    
+    SpitterController controller = new SpitterController(mockRepository);
+    MockMvc mockMvc = standaloneSetup(controller).build();
+    
+    mockMvc.perform(post("/spitter/register"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("registerForm"))
+        .andExpect(model().errorCount(5))
+        .andExpect(model().attributeHasFieldErrors(
+            "spitter", "firstName", "lastName", "username", "password", "email"));
+  }
+
+}
