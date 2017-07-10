045fcc2f2584519b99e09cd389a6a7b4f271a2b3
raiden <peckwood@users.noreply.github.com>
2017-07-10 15:02:43 +0800

    tried webAppContextSetup

--- src/test/java/spittr/web/SpitterControllerTest.java
+++ src/test/java/spittr/web/SpitterControllerTest.java
@@ -1,33 +1,46 @@
 package spittr.web;
 
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
 import spittr.Spitter;
 import spittr.config.RootConfig;
 import spittr.config.WebConfig;
 import spittr.data.SpitterRepository;
-import spittr.web.SpitterController;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(classes={RootConfig.class, WebConfig.class})
 public class SpitterControllerTest {
+	
+	@Autowired
+	private WebApplicationContext context;
 
   @Test
   public void shouldShowRegistration() throws Exception {
     SpitterRepository mockRepository = mock(SpitterRepository.class);
     SpitterController controller = new SpitterController(mockRepository);
-    MockMvc mockMvc = standaloneSetup(controller).build();
+   // MockMvc mockMvc = standaloneSetup(controller).build();
+    MockMvc mockMvc = webAppContextSetup(context).build();
     mockMvc.perform(get("/spitter/register"))
            .andExpect(view().name("registerForm"));
   }
@@ -57,8 +70,9 @@ public class SpitterControllerTest {
   @Test
   public void shouldFailValidationWithNoData() throws Exception {
     SpitterRepository mockRepository = mock(SpitterRepository.class);    
-    SpitterController controller = new SpitterController(mockRepository);
-    MockMvc mockMvc = standaloneSetup(controller).build();
+    //SpitterController controller = new SpitterController(mockRepository);
+    //MockMvc mockMvc = standaloneSetup(controller).build();
+    MockMvc mockMvc = webAppContextSetup(context).build();
     
     mockMvc.perform(post("/spitter/register"))
         .andExpect(status().isOk())
