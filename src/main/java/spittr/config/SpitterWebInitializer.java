3aca8ba360ba3431e17f24db397a5e6716e07b6a
raiden <peckwood@users.noreply.github.com>
2017-07-22 14:46:20 +0800

    added CharacterEncodingFilter in SpitterWebInitializer.java

--- src/main/java/spittr/config/SpitterWebInitializer.java
+++ src/main/java/spittr/config/SpitterWebInitializer.java
@@ -1,5 +1,8 @@
 package spittr.config;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 
@@ -18,6 +21,13 @@ public class SpitterWebInitializer extends AbstractAnnotationConfigDispatcherSer
   @Override
   protected String[] getServletMappings() {
     return new String[] { "/" };
+  }
+  
+  @Override
+  protected Filter[] getServletFilters() {
+    CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+    characterEncodingFilter.setEncoding("UTF-8");
+    return new Filter[] { characterEncodingFilter};
   }
 
 }
